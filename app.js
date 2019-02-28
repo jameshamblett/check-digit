0cadb7cbb50d10d68a3743d7aaf2749150a02038
jameshamblett <45458273+jameshamblett@users.noreply.github.com>
2019-02-28 23:03:56 +0000

    updated port issue

--- app.js
+++ app.js
@@ -25,6 +25,6 @@ app.post("/", function(req,res){
   res.render("index", {data:response});
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
